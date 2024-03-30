4dc37ef92f9df8ab01182c4b629f5bce72898b55
Dani Hoskins <hoskins.dannala@gmail.com>
2024-03-30 12:42:44 -0500

    Changes to js

--- quiz.js
+++ quiz.js
@@ -10,45 +10,8 @@
 // 10. Does the game prompt the player to visit the About page to send feedback?
 // 11. Does the contact form on About page submit an email to your prof email?
 
-
 // Answer to 1: Creates a random number
 const genNum = Math.floor(Math.random()* 10) +1;
+let guessCount = 0;
 console.log(genNum);
-
-let resetButton;
-
-// Answer to 2:
-// Answer to 3: 
-// Answer to 4:
-function checkAnswer() {
-
-   const userGuess = document.querySelector('.guess');
-   let sendButton = document.querySelector('.guessResults');
-   sendGuess.addEventListener('click', checkAnswer);
-  // let userAnswer = guessResults.value;
-   
-   if (userGuess === genNum){
-         console.log("You got it right! You are one super spy. The secret is: Anya's favorite food is peanuts.");
-         guessResults.textContent = "You got it right! You are one super spy. The secret is: Anya's favorite food is peanuts.";
-   } else if (userGuess > genNum) {
-         console.log('Your guess was too low. You are not a spy!');
-         guessResults.textContent = 'Your guess was too low. You are not a spy!';
-   } else if (userGuess < genNum){
-         console.log('Your guess was too high. You are not a spy!');
-         guessResults.textContent = 'Your guess was too high. You are not a spy!';
-   }
-
-  
-
-}
-
-
-// Answer to 5:
-// Answer to 6:
-// Answer to 7: 
-// Answer to 8:
-// Answer to 9:
-// Answer to 10:
-// Answer to 11:
-
 
