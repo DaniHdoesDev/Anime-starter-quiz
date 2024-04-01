c072c63e6fc7477385c59ed73ea3e3077de95bc2
Dani Hoskins <hoskins.dannala@gmail.com>
2024-03-31 20:06:07 -0500

    game working. external adjustment

--- quiz.js
+++ quiz.js
@@ -1,17 +1,37 @@
-// 1. Do you have a variable that generates a random number? Line 15
-// 2. Is the player's answer submitted with the submit button? 
-// 3. Can the player see how many guesses they have made?
-// 4. Do you have a function that checks the player's guess?
-// 5. Does the if else statement congratulate the player? 
-// 6. Does the function stop if the player is correct?
-// 7. Does the if else statement tell the player if they are incorrect?
-// 8. Does the game give the player a hint?
-// 9. Does game restart when the restart button is pressed?
-// 10. Does the game prompt the player to visit the About page to send feedback?
-// 11. Does the contact form on About page submit an email to your prof email?
 
+
+
+// 1. Do you have a variable that generates a random number? Line 13
 // Answer to 1: Creates a random number
-function checkAnswer(){
-    const genNum = Math.floor(Math.random()* 10) +1;
+
+function beginGame(){
+   const genNum = Math.floor(Math.random()* 10) +1;
+
     console.log(genNum);
+
+    document.getElementById('result').textContent = '';   
+
+
+// Answer to 2: Counts the number of guesses
+    const userGuess = parseInt(document.getElementById('guessInput').value);
+
+    if(isNaN(userGuess)|| userGuess < 1 || userGuess > 10){
+        document.getElementById('result').textContent = 'Please enter a number valid number.';
+    } else if(userGuess === genNum){
+        document.getElementById('result').textContent = 'Congratulations! You are a true spy.';
+    } else if(userGuess < genNum){
+        document.getElementById('result').textContent = 'Wrong! Try a higher number.';
+    } else {
+        document.getElementById('result').textContent = 'Wrong! Try a lower number.';
+    }
 }
+
+
+//To activate reset button
+function resetGame(){
+    beginGame();
+    document.getElementById('guessInput').value = '';
+}
+
+
+
